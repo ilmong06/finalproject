6b07530257df93edbe1f84f6b98b9d57305a4e98
vansshoes5 <kdh5532130@gmail.com>
2025-05-01 23:43:56 +0900

    change in voice_recog_UI

--- app/build/generated/source/buildConfig/debug/com/example/wav2vecapp/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/wav2vecapp/BuildConfig.java
@@ -9,4 +9,6 @@ public final class BuildConfig {
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String FLASK_BASE_URL = "http:///222.118.128.214";
 }
