ab65f0e384c9e38a41d888a0ad3d18ab4cdc7ee0
vansshoes5 <kdh5532130@gmail.com>
2025-05-27 16:31:20 +0900

    마이페이지 연결, 연결 전 사용자 정보 입력 페이지 추가

--- app/src/main/java/com/example/wav2vecapp/MyPageActivity.java
+++ app/src/main/java/com/example/wav2vecapp/MyPageActivity.java
@@ -1,6 +1,15 @@
 package com.example.wav2vecapp;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+
+    }
 }
