23bb764cf2093614cf474015b71f5c1b40eefa7c
vansshoes5 <kdh5532130@gmail.com>
2025-06-06 23:53:27 +0900

    수정사항 딱히 없음

--- app/src/main/java/com/example/wav2vecapp/KeywordRequest.java
+++ app/src/main/java/com/example/wav2vecapp/KeywordRequest.java
@@ -1,7 +1,12 @@
 package com.example.wav2vecapp;
 
+
+
+
+/// KeywordResponse.Java와 한 묶음으로 사용되는 클래스(단일 키워드 조회)
 public class KeywordRequest {
     private String uuid;
+
     public KeywordRequest(String uuid) {
         this.uuid = uuid;
     }
